5f15e000eae9bf74635f4eac121a1f8436c4e708
J.Shin <bonanob@hotmail.com>
2021-05-02 08:12:56 +0200

    added 404, projects names in dropdown menu

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import render_template, redirect, url_for, request
+from models import db, Project, app
+
+
+@app.route('/')
+def index():
+    all_projects = Project.query.all()
+    return render_template('index.html', projects=all_projects)
+
+
+@app.route('/about')
+def about():
+    all_projects = Project.query.all()
+    return render_template('about.html', projects=all_projects)
+
+
+@app.route('/projects/new')
+def new():
+    all_projects = Project.query.all()
+    return render_template('projectform.html', projects=all_projects)
+
+
+@app.route('/projects/<id>')
+def detail(id):
+    all_projects = Project.query.all()
+    project = Project.query.get(id)
+    return render_template('detail.html', projects=all_projects, id=project.id)
+
+
+@app.route('/projects/<id>/edit')
+def edit(id):
+    all_projects = Project.query.all()
+    return render_template('index.html', projects=all_projects)
+
+
+@app.route('/project/<id>/delete')
+def delete(id):
+    return render_template('index.html')
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("404.html", msg=error), 404
+
+
+if __name__ == '__main__':
+    db.create_all()
+    project1 = Project(title="Number Guessing Game",
+                       description="""
+                            Objective: Guess a number 1-10 with the lowest number of tries.<br>
+                            
+                            Process:<br>
+                            Guess a number between 1 and 10.
+                            After every answer, a message is shown whether the guess is too high or too low.
+                            If the guess is right, the game is done.\n\n\n
+                            Player are asked if they want to play again.\n\n\n
+                            
+                            Notes:\n
+                            Player inputs are validated(An int 1-10)
+                            At the start of each game, the current high score (least amount of points) is shown, so that players know what they're supposed to beat.""",
+                       skills="python",
+                       url="https://github.com/bonanob/number_guessing_game")
+    new_projects = [project1, project1]
+    db.session.add_all(new_projects)
+    db.session.commit()
+    app.run(debug=True, port=8000, host='127.0.0.1')
---
+++ models.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///projects.db"
+db = SQLAlchemy(app)
+
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column("Created", db.DateTime)
+    title = db.Column("Title", db.String())
+    description = db.Column("Description", db.Text)
+    skills = db.Column("Skills Practiced", db.Text)
+    url = db.Column("URL", db.String())
+
+    def __repr__(self):
+        return f"""<Project (Title: {self.title}
+                Created: {self.created}
+                Description: {self.description}
+                Skills Practiced: {self.skills}
+                URL: {self.url}
+                Tag: {self.url_alt}
+                )"""
